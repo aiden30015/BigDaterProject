08e45b82fc4e7008bf279063e0dd1f7159ac34ef
aiden30015 <loolloo080313@gmail.com>
2025-06-24 20:58:19 +0900

    ⚡️ :: 데이터 모델 변경

--- lib/providers/prediction_provider.dart
+++ lib/providers/prediction_provider.dart
@@ -1,5 +1,6 @@
 import 'package:big_dater_project/services/prediction_service.dart';
 import 'package:big_dater_project/models/price_date_model.dart';
+import 'package:big_dater_project/models/predict_model.dart';
 import 'package:flutter/material.dart';
 import 'package:riverpod/riverpod.dart';
 
@@ -8,12 +9,14 @@ class PredictionState {
   final bool isLoading;
   final String? error;
   final DateTimeRange dateRange;
+  final List<Predictions> predictions;
 
   PredictionState({
     this.priceDateModel,
     required this.isLoading,
     required this.dateRange,
     this.error,
+    this.predictions = const [],
   });
 
   PredictionState copyWith({
@@ -21,12 +24,14 @@ class PredictionState {
     bool? isLoading,
     String? error,
     DateTimeRange? dateRange,
+    List<Predictions>? predictions,
   }) {
     return PredictionState(
       priceDateModel: priceDateModel ?? this.priceDateModel,
       isLoading: isLoading ?? this.isLoading,
       error: error,
       dateRange: dateRange ?? this.dateRange,
+      predictions: predictions ?? this.predictions,
     );
   }
 }
@@ -43,7 +48,7 @@ class PredictionNotifier extends StateNotifier<PredictionState> {
             end: DateTime(2024, 12, 31),
           ),
         )) {
-    fetch();
+    fetchLstmPredictions();
   }
 
   Future<void> fetch() async {
@@ -56,6 +61,47 @@ class PredictionNotifier extends StateNotifier<PredictionState> {
         isLoading: false,
         error: null,
       );
+    } catch (e) {
+      state = state.copyWith(isLoading: false, error: e.toString());
+    }
+  }
+
+  Future<void> fetchPredictions(DateTime startDate, DateTime endDate) async {
+    state = state.copyWith(isLoading: true, error: null);
+    try {
+      final predictions = await _service.fetchPredictions(startDate, endDate);
+      state = state.copyWith(
+        predictions: predictions,
+        dateRange: DateTimeRange(start: startDate, end: endDate),
+        isLoading: false,
+        error: null,
+      );
+    } catch (e) {
+      state = state.copyWith(isLoading: false, error: e.toString());
+    }
+  }
+
+  Future<void> fetchLstmPredictions() async {
+    state = state.copyWith(isLoading: true, error: null);
+    try {
+      final predictions = await _service.fetchLstmPredictions();
+      
+      if (predictions.isNotEmpty) {
+        final startDate = predictions.first.date;
+        final endDate = predictions.last.date;
+        
+        state = state.copyWith(
+          predictions: predictions,
+          dateRange: DateTimeRange(start: startDate, end: endDate),
+          isLoading: false,
+          error: null,
+        );
+      } else {
+        state = state.copyWith(
+          isLoading: false,
+          error: "예측 데이터를 가져올 수 없습니다.",
+        );
+      }
     } catch (e) {
       state = state.copyWith(isLoading: false, error: e.toString());
     }
